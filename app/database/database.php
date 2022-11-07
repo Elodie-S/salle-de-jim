e80ee066566a05bfec9f11391ebccfffa12e3be5
Elodie Salvi <elodie@salvi.fr>
2022-11-08 00:24:51 +0100

    update DB

--- app/database/database.php
+++ app/database/database.php
@@ -1,7 +1,7 @@
 <?php
  
 try {
-        $db = new PDO('mysql:host=localhost;dbname=jim;charset=utf8', 'root', '');
+        $db = new PDO('mysql:host=ltnya0pnki2ck9w8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=lsw26g66z391bhel;charset=utf8', 'yt76qq6mqmsf72by', 'uabt1bui24jncbs0');
 } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
 }
