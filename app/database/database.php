6218f45ea44cd0e4335b1a91fc6a802e21b3734f
Elodie Salvi <elodie@salvi.fr>
2022-11-08 00:36:01 +0100

    db

--- app/database/database.php
+++ app/database/database.php
@@ -1,7 +1,7 @@
 <?php
  
 try {
-        $db = new PDO('mysql:host=ltnya0pnki2ck9w8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=lsw26g66z391bhel;charset=utf8', 'yt76qq6mqmsf72by', 'uabt1bui24jncbs0');
+        $db = new PDO('mysql:host=ltnya0pnki2ck9w8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=ep2ei3228lp2y3ym;charset=utf8', 'vzots9qgk41z3qvr', 'zjc7k8pl6dbw7imk');
 } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
 }
