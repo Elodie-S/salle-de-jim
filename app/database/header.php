c720a5fe60c94f9c28059c4502c3d1483dfe976b
Elodie Salvi <elodie@salvi.fr>
2022-11-08 14:36:47 +0100

    routes admin

--- app/database/header.php
+++ app/database/header.php
@@ -1,6 +1,6 @@
 <?php
-require_once '/app/database/database.php';
-require_once '/app/database/security.php';
+require_once ''.__DIR__ .'/app/database/database.php';
+require_once ''.__DIR__ .'/app/database/security.php';
 ?>
 
 <!DOCTYPE html>
